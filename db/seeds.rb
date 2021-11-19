cd41a0b7d038dbd6e8efff70a8dcae3ff41c86ef
Sanju Basnet <sanjubasnet036@gmail.com>
2021-11-19 15:12:14 +0545

    list of data manually created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Post.create(title: 'material design', description:"Material Design is a design language introduced by google in 2014. 
-                                                   So, we can say it is the google-promoted design. It is a biggest design
-                                                    product happen in google. It have a sound browser compatibility as they
-                                                     are compatible across most browser. Material design support angular 
-                                                     material and react material user interface. It also uses SASS preprocessor.
-                                                      It is a simple design system into 3D. The design are so awesome that 
-                                                      naturally attract human eye. Simply, in one word material design is a 
-                                                      logic with a magic.")
+5.times do |i|
+    Post.create(title: "Post Number #{i}", description: "This is the body of post number #{i}")
+   end
