89875ff1b075fb6ff8e351f8ed0f9e674764b63d
Sanju Basnet <sanjubasnet036@gmail.com>
2021-12-16 16:20:26 +0545

    routes for session controller, devise and hotels

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       defaults format: :json do
+        post :sign_in, to: 'sessions#create'
         resources :posts
+        resources :hotels
       end
     end   
   end
